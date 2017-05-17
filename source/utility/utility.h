4f60b9d15d3a23e5a43a98798e2013132c03c56b
TomatoPi <dago.esaie@outlook.fr>
2017-05-17 22:16:48 +0200

    Added Comments

--- source/utility/utility.h
+++ source/utility/utility.h
@@ -7,36 +7,43 @@
 
 #include <math.h>
 #include <jack/jack.h>
-/*
-*	Simplification de l'ecriture du type jack_default_audio_sample_t
-*/
+
+//Simplification de l'ecriture du type jack_default_audio_sample_t
 typedef jack_default_audio_sample_t sample_t;
 
 int fx_Process_Callback(jack_nframes_t nframes, void *u);
 
+/*
+*	Classe de base de tout les modules audio du programme
+*/
 class Fx_Client{
 	
 	public:
 	
 		Fx_Client(const char *server, const char *name);
 		
-		virtual int process(jack_nframes_t nframes, void *arg){};
+		virtual int process(jack_nframes_t nframes, void *arg){}; // Fonction callback du client
 		
 	protected:
 	
-		jack_client_t *client;
-		char* name;
+		jack_client_t *client;	//JACK Client
+		char* name;		//Client unique name
 };
 
 /*
-*	Fonction qui contraint un sample "in" dans un interval donné [min, max]
+*	Fonction qui contraint un sample "in" dans un interval donné [min, max] ( HardClipping equivalent )
 */
 sample_t sfx_clip(sample_t in, float min, float max);
-sample_t sfx_soft(sample_t in, float max, float soft, float shape);
-sample_t sfx_abs(sample_t in);
 
-float sfx_dbtorms(float d);
-float sfx_rmstodb(float g);
+/*
+*	Fonction qui contraint un sample "in" dans un interval donné, avec adoucissement du coude ( SoftClipping equivalent )
+*/
+sample_t sfx_soft(sample_t in, float max, float soft, float shape);
+
+sample_t sfx_abs(sample_t in); // Fonction valeur absolue
+
+float sfx_dbtorms(float d); //conversion db vers un gain
+float sfx_rmstodb(float g); //conversion d'un gain en db
 
 static float vsa = (1.0 / 4294967295.0); //Très petite valeur
 
@@ -47,14 +54,14 @@ typedef struct
 {
 
 	//Filtre passe bas
-	float fl; 	// Frequence
+	float fl; 	// Frequence du filtre
 	float f1p0;// Poles
 	float f1p1; 
 	float f1p2;
 	float f1p3;
 
 	// Filtre pass haut
-	float fh; // Frequence
+	float fh; // Frequence du filtre
 	float f2p0; // Poles
 	float f2p1;
 	float f2p2;
@@ -74,7 +81,21 @@ typedef struct
 
 } sfx_tripole; 
 
+/*
+*	Initialisation d'un EQ trois poles
+*	f : EQ
+*	fl : frequence basse
+* 	fh : frequence haute
+*	sr : current samplerate
+*	gl : gain bande basse
+*	gm : gain bande medium
+*	gh : gain bande haute
+*/
 void sfx_init_tripole(sfx_tripole *f, int fl, int fh, int sr, float gl, float gm, float gh);
+
+/*
+*	Egalisation du sample donné
+*/
 sample_t sfx_do_tripole(sfx_tripole* f, sample_t sample);
 
 #endif
