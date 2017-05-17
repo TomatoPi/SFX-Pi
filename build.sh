f32ac6b2013eeda0a4fd2cda01c6e9ba85c407f7
TomatoPi <dago.esaie@outlook.fr>
2017-05-18 01:50:25 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Software Space-FX : Compilation launched"
 
-g++ ./source/SFX_Pi.cpp ./source/utility/utility.cpp ./source/fx/drive.cpp ./source/fx/Fx_Unit.cpp ./source/mod/LFO.cpp -I ./source `pkg-config --cflags --libs jack` -o ./SFX_Pi
+g++ ./source/SFX_Pi.cpp ./source/core/Utility.cpp ./source/core/Module.cpp ./source/modules/Drive.cpp ./source/modules/LFO.cpp -I ./source `pkg-config --cflags --libs jack` -o ./SFX_Pi
 
 echo "Compilation completed successfully"
