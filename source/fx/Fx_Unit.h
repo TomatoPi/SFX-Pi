833e5205a5b643f0d89ffd63b938431a54a70cc5
TomatoPi <dago.esaie@outlook.fr>
2017-05-17 21:42:05 +0200

    Added comments

--- source/fx/Fx_Unit.h
+++ source/fx/Fx_Unit.h
@@ -23,9 +23,14 @@ class Fx_Unit : public Fx_Client{
 		virtual int process(jack_nframes_t nframes, void *arg){};
 		
 	protected:
-		jack_port_t **a_port;
+		jack_port_t **a_port; //io du client
 };
 
+/*
+* 	FX de drive, Soft Clip, Hard clip
+*	Filtrage trois bande avant le clip
+*	Possibilité de clipping asymetrique
+*/
 class Drive : public Fx_Unit{
 	
 	public:
@@ -36,16 +41,17 @@ class Drive : public Fx_Unit{
 	
 	protected :
 		
-		float gp, gn;
-		float sp, sn;
-		float shp, shn;
-		sfx_tripole *filter;
+		float gp, gn; //Gain appliqué au signal, positif - negatif
+		float sp, sn; // Softness du soft clip, positif - negatif
+		float shp, shn; //Shape -----------------------------
+		sfx_tripole *filter; // Filtre trois bandes ( bass, mid, high )
 		
-		int is_abs;
-		int is_asm;
-		int is_soft_clip_p;
-		int is_soft_clip_n;
+		int is_abs; //Si redressage double alternace du signal
+		int is_asm; //Si clipping asymetrique : paramètres _p utilisés pour les alternaces positives, _n pour les alternaces negatives
+		int is_soft_clip_p, is_soft_clip_n; //si le clipping est soft ou hard
 };
 
-class Fx_Mod : public 
+/*class Fx_Mod : public Fx_Client{
+	
+};*/
 #endif
