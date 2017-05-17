e4b71f1f9b129376a8ee2232a75390598f7ad48e
TomatoPi <dago.esaie@outlook.fr>
2017-05-18 00:42:15 +0200

    Update Modules.h

--- source/core/Modules.h
+++ source/core/Modules.h
@@ -21,13 +21,6 @@ class Module{
 	
 		jack_client_t *client;	//JACK Client
 		char* name;		//Client unique name
-	
-	private:
-		/*
-		*	Create and register ports for this Module
-		*	Takes number of ports and arrays of port names, port types, port flags
-		*/
-		void mod_Register_Port(int port_count, const char **port_names, const char **port_types, unsigned long **port_flags);
 };
 
 #endif
