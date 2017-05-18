d665be9d0f41b2306cb4409a59255b8d20b5b1b6
TomatoPi <dago.esaie@outlook.fr>
2017-05-18 23:31:00 +0200

    Update Drive.cpp

--- source/modules/Drive.cpp
+++ source/modules/Drive.cpp
@@ -4,8 +4,7 @@
 *	Distortion effect constructor
 *	EQ creation
 */
-Drive::Drive(const char *server, const char *name): Module(server, name, 2, 2, 0, 0, "in_L", "in_R", "out_L", "out_R")
-                                                    , gp(70), gn(30), sp(10), sn(5), shp(0.29), shn(0.8), is_abs(0), is_asm(1), is_soft_clip_p(1), is_soft_clip_n(0){
+Drive::Drive(const char *server, const char *name): Module(server, name, 2, 2, 0, 0, "in_L", "in_R", "out_L", "out_R"), params_count(10){
 	
 	this->filter = (spi_tripole*)malloc(sizeof(spi_tripole));
 	spi_init_tripole(this->filter, 200, 1000, (int)jack_get_sample_rate(this->client), 0.75, 3.0, 7.0);
@@ -14,6 +13,25 @@ Drive::Drive(const char *server, const char *name): Module(server, name, 2, 2, 0
 		fprintf (stderr, "Failed activating Client\n");
 		exit (1);
 	}
+	
+	this->params = (float*)malloc(this->params_count * sizeof(flaot));
+	if(this->params == NULL){
+		fprintf(stderr, "Failed Create Params list\n");
+		exit(1);
+	}
+	
+	this->params[0] = D_ABS;
+	this->params[1] = D_ASM;
+	
+	this->params[2] = D_GAIN;
+	this->params[3] = D_SOFT;
+	this->params[4] = D_SHAPE;
+	this->params[5] = D_TYPE;
+	
+	this->params[6] = D_GAIN;
+	this->params[7] = D_SOFT;
+	this->params[8] = D_SHAPE;
+	this->params[9] = D_TYPE;
 }
 
 /*
@@ -29,55 +47,53 @@ int Drive::process(jack_nframes_t nframes, void *arg){
 	s_in_r = (sample_t*)jack_port_get_buffer(this->port[1], nframes);
 	s_out_r = (sample_t*)jack_port_get_buffer(this->port[3], nframes);
 	
-	float g1 = this->gp, g2 = this->gn; //collecting positive & negative gains
+	float gp, sp, shp, gn, sn, shn;			//Collecting drive settings
+	int is_abs, isp, isn;
+	is_abs = (int)this->params[0];
+	int n = (this->params[1] == 0.0)?0:4;
+	
+	gp = this->params[2];
+	sp = this->params[3];
+	shp = this->params[4];
+	isp = (int)this->params[5];
+	
+	gn = this->params[2+n];
+	sn = this->params[3+n];
+	shn = this->params[4+n];
+	isn = (int)this->params[5+n];
 	
 	for(jack_nframes_t i = 0; i < nframes; i++){						//For each sample
 		
 		sample_t l = spi_do_tripole(this->filter, s_in_l[i]);				//Current Left sample
 		sample_t r = spi_do_tripole(this->filter, s_in_r[i]);				//Current Right sample
-		
-		if(this->is_asm){ 									//if asymmetrical clipping
-														//Left Channel
-			if(l>0){ 										//if positive
-				if(this->is_soft_clip_p){								//soft-clipping or hard-clipping
-					s_out_l[i] = spi_soft(l*g1, 0.5, this->sp, this->shp);					//soft-clipping
-				}else{											//else
-					s_out_l[i] = spi_clip(l*g1, -0.5, 0.5);							//hard-clipping
-				}																
-			}else{											//if negative	
-				if(this->is_soft_clip_n){                                      				//soft-clipping or hard-clipping
-					s_out_l[i] = spi_soft(l*g2, 0.5, this->sn, this->shn);          			//soft-clipping
-				}else{                                                              			//else
-					s_out_l[i] = spi_clip(l*g2, -0.5, 0.5);                         			//hard-clipping
-				}
+											//Left Channel
+		if(l>0){ 										//if positive
+			if(isp){								//soft-clipping or hard-clipping
+				s_out_l[i] = spi_soft(l*gp, 0.5, sp, shp);					//soft-clipping
+			}else{											//else
+				s_out_l[i] = spi_clip(l*gp, -0.5, 0.5);							//hard-clipping
+			}																
+		}else{											//if negative	
+			if(isn){                                      				//soft-clipping or hard-clipping
+				s_out_l[i] = spi_soft(l*gn, 0.5, sn, shn);          			//soft-clipping
+			}else{                                                              			//else
+				s_out_l[i] = spi_clip(l*gn, -0.5, 0.5);                         			//hard-clipping
 			}
-			if(r>0){										//Same for Right Channel
-				if(this->is_soft_clip_p){
-					s_out_r[i] = spi_soft(r*g1, 0.5, this->sp, this->shp);
-				}else{
-					s_out_r[i] = spi_clip(r*g1, -0.5, 0.5);
-				}
-			}else{
-				if(this->is_soft_clip_n){
-					s_out_r[i] = spi_soft(r*g2, 0.5, this->sn, this->shn);
-				}else{
-					s_out_r[i] = spi_clip(r*g2, -0.5, 0.5);
-				}
-			}
-		}else{											//if symmetrical clipping
-			if(this->is_soft_clip_p){								//soft-clipping or hard-clipping
-					s_out_l[i] = spi_soft(l*g1, 0.5, this->sp, this->shp);				//soft-clipping
-				}else{										//else
-					s_out_l[i] = spi_clip(l*g1, -0.5, 0.5);						//hard-clipping
-				}
-			if(this->is_soft_clip_p){							//Same for Right Channel
-					s_out_r[i] = spi_soft(r*g1, 0.5, this->sp, this->shp);
-				}else{
-					s_out_r[i] = spi_clip(r*g1, -0.5, 0.5);
-				}
-				
 		}
-		if(this->is_abs){									//if absolute value mode is true, full-wave rectification
+		if(r>0){										//Same for Right Channel
+			if(isp){
+				s_out_r[i] = spi_soft(r*gp, 0.5, sp, shp);
+			}else{
+				s_out_r[i] = spi_clip(r*gp, -0.5, 0.5);
+			}
+		}else{
+			if(isn){
+				s_out_r[i] = spi_soft(r*gn, 0.5, sn, shn);
+			}else{
+				s_out_r[i] = spi_clip(r*gn, -0.5, 0.5);
+			}
+		}
+		if(is_abs){									//if absolute value mode is true, full-wave rectification
 			s_out_l[i] = spi_abs(s_out_l[i]);
 			s_out_r[i] = spi_abs(s_out_r[i]);
 		}
