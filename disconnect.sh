f38a46e3eec559568145c87ae0b6afdd041e4cc3
TomatoPi <dago.esaie@outlook.fr>
2017-05-18 05:15:48 +0200

    Update disconnect.sh

--- disconnect.sh
+++ disconnect.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Deconnection du client FX" $1 " au serveur Space_FX"
 
-jack_disconnect --server Space_FX system:capture_1 $1:in_l
-jack_disconnect --server Space_FX system:capture_1 $1:in_r
+jack_disconnect --server Space_FX system:capture_1 $1:in_L
+jack_disconnect --server Space_FX system:capture_1 $1:in_R
 
-jack_disconnect --server Space_FX $1:out_l system:playback_1
-jack_disconnect --server Space_FX $1:out_r system:playback_2
+jack_disconnect --server Space_FX $1:out_L system:playback_1
+jack_disconnect --server Space_FX $1:out_R system:playback_2
