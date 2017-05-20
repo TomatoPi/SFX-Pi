4c288a1fc4c4a42a55f91d371ba0a00dff9b2722
TomatoPi <dago.esaie@outlook.fr>
2017-05-20 08:38:04 +0200

    Update LFO.cpp

--- source/modules/LFO.cpp
+++ source/modules/LFO.cpp
@@ -45,6 +45,13 @@ int LFO::process(jack_nframes_t nframes, void *arg){
 	return 0;
 }
 
+int Module::bypass(jack_nframes_t nframes, void *arg){
+	
+	sample_t *out = (sample_t*)jack_port_get_buffer(this->port[0], nframes);
+	memset(out, 0.0, sizeof(sample_t) * nframes);
+	return 0;
+}
+
 void lfo_set_type(LFO *lfo, LFO_Wave type){
 	lfo->params[0] = type;
 	switch(type){
