0e3dfd8ee23135594ed3b043fc10c1524bd38719
TomatoPi <dago.esaie@outlook.fr>
2017-05-20 10:55:45 +0200

    Update Modules.cpp

--- source/core/Modules.cpp
+++ source/core/Modules.cpp
@@ -10,11 +10,11 @@ int mod_Bypass_Callback(jack_nframes_t nframes, void *u){
 	return static_cast<Module*>(u)->bypass(nframes, u);
 }
 
-int mod_GenericStereoBypass_Callback(jack_nframes_t nframes, jack_port_t **ports){
+int mod_GenericStereoBypass_Callback(jack_nframes_t nframes, jack_port_t **ports, int foi){
 	
 	for(int i = 0; i < 2; i++){
 		sample_t *in = (sample_t*)jack_port_get_buffer(ports[i], nframes);
-		sample_t *out = (sample_t*) jack_port_get_buffer(ports[i+2], nframes);
+		sample_t *out = (sample_t*) jack_port_get_buffer(ports[foi+i], nframes);
 		memcpy(out, in, sizeof(sample_t) * nframes);
 	}
 	return 0;
