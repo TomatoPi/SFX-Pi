95b1657afb4b7ee9a79fc95d18e9625f4cdea977
TomatoPi <dago.esaie@outlook.fr>
2017-05-25 01:44:34 +0200

    Create Delay.h

---
+++ source/modules/Delay.h
@@ -0,0 +1,12 @@
+#ifndef DEF_DELAY_H
+#define DEF_DELAY_H
+
+#include "../core/Modules.h"
+#include "../core/Utility.h"
+#include "../core/Ringbuffer.h"
+
+class Delay : public Module{
+
+};
+
+#endif
