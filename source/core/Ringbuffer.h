1fcda409560ab431f6b58d208e9d47f7945b939e
TomatoPi <dago.esaie@outlook.fr>
2017-05-24 09:42:14 +0200

    Update Ringbuffer.h

--- source/core/Ringbuffer.h
+++ source/core/Ringbuffer.h
@@ -5,12 +5,29 @@ class Ringbuffer{
 
 	public :
 	
-		Ringbuffer(int length);
+		/*
+		*	Create a ring buffer
+		*	length : ringbuffer's lenght in ms
+		*	samplerate : samplerate in Hz
+		*/
+		Ringbuffer(int length, int samplerate);
 		
+		/*
+		*	Read the block starting at (writing head) - delay
+		*/
+		int read_block(float *block, int delay, int length);
+	
+		/*
+		*	Write a new block and move the head
+		*/
+		int write_block(float *block, int length);
 	
 	protected :
 		float *buffer;
+	
 		int buffer_size;
+		int samplerate;
+	
 		int write_head;
 };
 
