8be3feabe50250a2e6963108589f7e92bb0cf012
TomatoPi <dago.esaie@outlook.fr>
2017-05-25 10:08:21 +0200

    Update Ringmod.cpp

--- source/modules/Ringmod.cpp
+++ source/modules/Ringmod.cpp
@@ -1,8 +1,9 @@
 #include "Ringmod.h"
 
-Ringmod::Ringmod(const char *server, const char *name): Module(server, name, 1, 3, 2, 0, 0, "in_L", "in_R", "in_M", "out_L", "out_R"){
+Ringmod::Ringmod(const char *server, const char *name): Module(server, name, 2, 3, 2, 0, 0, "in_L", "in_R", "in_M", "out_L", "out_R"){
 
 	this->params[0] = R_DEPTH;
+	this->params[1] = 1.0;
 	
 	if (jack_activate (this->client)) {
 		fprintf (stderr, "Failed activating Client\n");
@@ -23,11 +24,12 @@ int Ringmod::process(jack_nframes_t nframes, void *arg){
 	in_Mod = (sample_t*)jack_port_get_buffer(this->port[2], nframes);
 	
 	float depth = this->params[0];
+	float dw = this->params[1];
 	
 	for(jack_nframes_t i = 0; i < nframes; i++){
 		
-		out_L[i] = (in_L[i] * (1 - depth)) + (in_L[i] * in_Mod[i] * depth);
-		out_R[i] = (in_R[i] * (1 - depth)) + (in_R[i] * in_Mod[i] * depth);
+		out_L[i] = spi_dry_wet(in_L[i], (in_L[i] * (1 - depth)) + (in_L[i] * in_Mod[i] * depth), dw);
+		out_R[i] = spi_dry_wet(in_R[i], (in_R[i] * (1 - depth)) + (in_R[i] * in_Mod[i] * depth), dw);
 	}
 	return 0;
 }
