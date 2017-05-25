79ec4542fdf59933a0ae51033c947fd1ecf255f1
TomatoPi <dago.esaie@outlook.fr>
2017-05-25 10:27:33 +0200

    Update Delay.cpp

--- source/modules/Delay.cpp
+++ source/modules/Delay.cpp
@@ -1,5 +1,5 @@
 #include "Delay.h"
-Delay::Delay(const char *server, const char *name) : Module(server, name, 5, 2, 2, 0, 0, "in_L", "in_R", "out_L", "out_R"){
+Delay::Delay(const char *server, const char *name) : Module(server, name, 6, 2, 2, 0, 0, "in_L", "in_R", "out_L", "out_R"){
     
     this->buffer_L = new Ringbuffer(D_DMAX, jack_get_sample_rate(this->client));
     this->buffer_R = new Ringbuffer(D_DMAX, jack_get_sample_rate(this->client));
@@ -17,6 +17,9 @@ Delay::Delay(const char *server, const char *name) : Module(server, name, 5, 2, 
     this->params[2] = D_DELAY;
     this->params[3] = D_FEED;
     this->params[4] = D_FEED;
+	
+		this->params[5] = 1.0;
+	
   
     if (jack_activate (this->client)) {
       fprintf (stderr, "Echec de l'activation du client\n");
@@ -36,17 +39,21 @@ int Delay::process(jack_nframes_t nframes, void *arg){
 	
 	float fl = this->params[3];
 	float fr = this->params[4];
+	float dw = this->params[5];
 
 	for(jack_nframes_t i = 0; i < nframes; i++){
 		
 		this->buffer_L->read_value(&(this->reader_L));
 		this->buffer_R->read_value(&(this->reader_R));
 			
-		out_L[i] = in_L[i] + this->reader_L.value;
-		out_R[i] = in_R[i] + this->reader_R.value;
+		sample_t wl = this->reader_L.value;
+		sample_t wr = this->reader_R.value;
 		
-		this->buffer_L->write_value(in_L[i] + fl*out_L[i]);
-		this->buffer_R->write_value(in_R[i] + fr*out_R[i]);
+		this->buffer_L->write_value(in_L[i] + fl*wl);
+		this->buffer_R->write_value(in_R[i] + fr*wr);
+		
+		out_L[i] = spi_dry_wet(in_L[i], wl, dw);
+		out_R[i] = spi_dry_wet(in_R[i], wr, dw);
 	}
 	return 0;
 }
