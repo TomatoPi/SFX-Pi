638c6c1fd0e227917fe95a9f2c61f36d88643618
TomatoPi <dago.esaie@outlook.fr>
2017-05-25 07:36:07 +0200

    Update Delay.h

--- source/modules/Delay.h
+++ source/modules/Delay.h
@@ -5,8 +5,32 @@
 #include "../core/Utility.h"
 #include "../core/Ringbuffer.h"
 
+#define D_DMAX 5000
+#define D_DELAY 250
+#define D_FEED 0.5
+
 class Delay : public Module{
 
+	public:
+		Delay(const char *server, const char *name);
+		
+		virtual int process(jack_nframes_t nframes, void *arg){};
+		virtual int bypass(jack_nframes_t nframes, void *arg){};
+	
+	protected:
+		
+		Ringbuffer *buffer_L, *buffer_R;
+		rng_reader *reader_L, *reader_R;
+	
+		/*
+		* Params are :
+		*
+		* max_delay
+		* delay_L
+		* delay_R
+		* feedback_L
+		* feedback_R
+		*/
 };
 
 #endif
