771d2d901a6d309b35aea3c179403f9ea25958aa
TomatoPi <dago.esaie@outlook.fr>
2017-05-26 13:36:05 +0200

    Update Reglages.h

--- source/io/Reglages.h
+++ source/io/Reglages.h
@@ -1,4 +1,17 @@
 #ifndef DEF_REGLAGES_H
 #define DEF_REGLAGES_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+#include <mcp3004.h>
+
+#define BASE 200
+#define SPI_CHAN1 0
+#define SPI_CHAN2 1
+
+void io_init_spi();
+
+int io_get_param(int param);
+
 #endif
