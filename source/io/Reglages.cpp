71200108cfd8c1655daefd3de03e99c85fd8ca11
TomatoPi <dago.esaie@outlook.fr>
2017-05-26 14:48:21 +0200

    Update Reglages.cpp

--- source/io/Reglages.cpp
+++ source/io/Reglages.cpp
@@ -1,1 +1,23 @@
 #include "Reglages.h"
+
+void io_init_spi(){
+  
+	int chan;
+	int x;
+	printf ("Raspberry Pi wiringPi test program\n") ;
+
+	if (wiringPiSetup () == -1)
+	  exit (1) ;
+
+	mcp3004Setup (BASE, SPI_CHAN);
+
+	for (chan = 0 ; chan < 8 ; ++chan) {
+		
+	  	x = analogRead (BASE + chan) ;
+		printf("%d\n", x);
+	}
+}
+
+int io_get_param(int param){
+  
+}
