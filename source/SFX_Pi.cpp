7ba8322aeb8de0308acc905ff5f9b4e0316e1b62
TomatoPi <dago.esaie@outlook.fr>
2017-05-26 13:37:17 +0200

    Update SFX_Pi.cpp

--- source/SFX_Pi.cpp
+++ source/SFX_Pi.cpp
@@ -11,12 +11,6 @@
 #include "./modules/Ringmod.h"
 #include "./modules/Delay.h"
 
-#include <wiringPi.h>
-#include <mcp3004.h>
-
-#define BASE 200
-#define SPI_CHAN 0
-
 int SAMPLERATE = 48000;
 const char *SERVER_NAME = "Space_Fx";
 
@@ -28,20 +22,5 @@ int main(int argc, char *argv[]){
 	Ringmod test3 = Ringmod(SERVER_NAME, (char*)"RG");
 	Delay test4 = Delay(SERVER_NAME, (char*)"DL");
 	
-	int chan;
-  	int x;
-	printf ("Raspberry Pi wiringPi test program\n") ;
-
- 	if (wiringPiSetup () == -1)
-    		exit (1) ;
-
-  	mcp3004Setup (BASE, SPI_CHAN); // 3004 and 3008 are the same 4/8 channels
-
-  	for (chan = 0 ; chan < 8 ; ++chan) {
-    		x = analogRead (BASE + chan) ;
-  		printf("%d\n", x);
-    	}
-   	return 0 ;
-	
-//	sleep(-1);
+	sleep(-1);
 }
