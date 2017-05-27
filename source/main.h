738d24e4579d64807ce8bfccb364b43afc4cf311
TomatoPi <dago.esaie@outlook.fr>
2017-05-27 16:45:44 +0200

    Update main.h

--- source/main.h
+++ source/main.h
@@ -1,6 +1,23 @@
 #ifndef DEF_MAIN_H
 #define DEF_MAIN_H
 
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include <unordered_map>
+
+#include <jack/jack.h>
+
+#include "./modules/Drive.h"
+#include "./modules/LFO.h"
+#include "./modules/Ringmod.h"
+#include "./modules/Delay.h"
+
+int main_add_module(MODULE_TYPE mod);
+
+int main_add_connection(Module *source, int is, Module *destination, int id);
 
 #endif
