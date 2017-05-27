bb9dceca9d9e8c450564420cf4b9f356ac747861
TomatoPi <dago.esaie@outlook.fr>
2017-05-27 10:41:57 +0200

    Update and rename Reglages.h to io.h

--- source/io/io.h
+++ source/io/io.h
@@ -1,14 +1,15 @@
-#ifndef DEF_REGLAGES_H
-#define DEF_REGLAGES_H
+#ifndef DEF_IO_H
+#define DEF_IO_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <wiringPi.h>
 #include <mcp3004.h>
 
-#define BASE 100
+#define SPI_BASE 100
 #define SPI_CHAN1 0
-#define SPI_CHAN2 1
+#define SPI_PMAX 8
+#define SPI_MAX 1023
 
 void io_init_spi();
 
