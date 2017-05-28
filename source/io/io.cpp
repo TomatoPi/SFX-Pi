e21cc46a5a461620c85f0152428961bdacd762c3
TomatoPi <dago.esaie@outlook.fr>
2017-05-28 09:57:02 +0200

    Update io.cpp

--- source/io/io.cpp
+++ source/io/io.cpp
@@ -15,3 +15,29 @@ int io_get_potentiometer(int potentiometer){
 	if(potentiometer < 8) return analogRead(SPI_BASE + potentiometer);
 	return -1;
 }
+
+int io_set_param_accessor(io_param_accessor *accessor, int potentiometer, float min, float max, float *target, int is_db){
+	
+	if(potentiometer < 0 || potentiometer > 7 || target == NULL)
+		return 1;
+	
+	accessor->potentiometer = potentiometer;
+	accessor->min = min;
+	accessor->max = max;
+	accessor->target = target;
+	accessor->is_db = is_db;
+	
+	return 0;
+}
+
+void io_update_param(io_param_accessor *accessor){
+	
+	float value = (float)io_get_potentiometer(accessor->potentiometer);
+	float param = ((value/SPI_MAX) * (accessor->max - accessor->min)) + accessor->min;
+	
+	if(is_db){
+		*(accessor->target) = spi_dbtorms(param);
+	}else{
+		*(accessor->target) = param;
+	}
+}
