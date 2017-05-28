2c8efc0ff105207e02b05af89a3dfec1254eb593
TomatoPi <dago.esaie@outlook.fr>
2017-05-28 15:10:43 +0200

    Update SFX_Pi.cpp

--- source/SFX_Pi.cpp
+++ source/SFX_Pi.cpp
@@ -13,22 +13,25 @@ int main(int argc, char *argv[]){
 	fprintf (stderr, "\nBienvenu dans le fantastique et magnifique software Space-FX\n\n------------------\n");
 	io_init_spi();
 	
-	int drive = main_add_module(MDRIVE);
-	float drivep[] = {0, 1, 20.0, 1, 8.0, 0.26, 10.0, 1, 10.0, 0.6}
+	main_add_module(MDRIVE);
+	int drive = MAIN_COUNT_MODULE;
+	float drivep[] = {0, 1, 20.0, 1, 8.0, 0.26, 10.0, 1, 10.0, 0.6};
 	MAIN_LIST_MODULE[drive]->set_param_list(10, drivep);
 	main_add_connection(NULL, 0, MAIN_LIST_MODULE[drive], 0);
 	main_add_connection(NULL, 0, MAIN_LIST_MODULE[drive], 1);
 	main_add_accessor(drive, 2, 0, 10.0, 40.0 ,1, 0);
 	main_add_accessor(drive, 6, 0, 10.0, 30.0, 1, 1);
 	
-	int lfo = main_add_module(MLFO);
-	float lfop[] = {5, 2.0, 48000, 0.0, 0.0, 0.0, 3, 16};
-	lfo_set_type(MAIN_LIST_MODULE[lfo], WAVE_NPH);
+	main_add_module(MLFO);
+	int lfo = MAIN_COUNT_MODULE;
+	float lfop[] = {5, 2.0, 48000, 0.0, 0.0, 0.0, 0.3, 0.7};
+	lfo_set_type((LFO*)(MAIN_LIST_MODULE[lfo]), WAVE_VAR);
 	MAIN_LIST_MODULE[lfo]->set_param_list(8, lfop);
 	main_add_accessor(lfo, 1, 1, 0.5, 10.0, 0, 0);
 	
-	int ringmod = main_add_module(MRINGM);
-	float ringmodp[] = {1.0, 1.0}
+	main_add_module(MRINGM);
+	int ringmod = MAIN_COUNT_MODULE;
+	float ringmodp[] = {1.0, 1.0};
 	MAIN_LIST_MODULE[ringmod]->set_param_list(2, ringmodp);
 	main_add_accessor(ringmod, 0, 2, 0.0, 1.0, 0, 0);
 	
@@ -41,7 +44,7 @@ int main(int argc, char *argv[]){
 	
 	while(1){
 	
-		unordered_map<string, double>:: iterator itr;
+		unordered_map<int, io_param_accessor*>:: iterator itr;
 		for (itr = MAIN_LIST_ACCESSOR.begin(); itr != MAIN_LIST_ACCESSOR.end(); itr++)
 		{
 			// type itr->first stores the key part  and
@@ -87,7 +90,7 @@ int main_add_module(MODULE_TYPE mod){
 	if(newmod==NULL) return 1;
 	
 	MAIN_LIST_MODULE.insert(make_pair(MAIN_COUNT_MODULE, newmod));
-	fprintf (stderr, "Added new module !\n");
+	fprintf (stderr, "Added new module : %d\n", MAIN_COUNT_MODULE);
 	
 	return 0;
 }
@@ -215,13 +218,14 @@ int main_add_accessor(int target, int param_idx, int potentiometer, float min, f
 	if(MAIN_LIST_MODULE[target]->get_param_adress(param_idx) == NULL){
 		fprintf (stderr, "\nParam target not found\n");
 		return 1;
+	}
 	
 	while(MAIN_LIST_ACCESSOR.find(MAIN_COUNT_ACCESSOR) != MAIN_LIST_ACCESSOR.end())
 		MAIN_COUNT_ACCESSOR++;
 	
-	io_param_accessor accessor = new io_param_accessor(potentiometer, min, max, MAIN_LIST_MODULE[target]->get_param_adress(param_idx), is_db, is_inv);
+	fprintf (stderr, "Idx : %d -- Target : %d.%d -- Pot : %d -- Min : %f -- Max : %f\n", MAIN_COUNT_ACCESSOR, target, param_idx, potentiometer, min, max);
+	io_param_accessor *accessor = new io_param_accessor(potentiometer, min, max, MAIN_LIST_MODULE[target]->get_param_adress(param_idx), is_db, is_inv);
 	MAIN_LIST_ACCESSOR.insert(make_pair(MAIN_COUNT_ACCESSOR, accessor));
-	fprintf (stderr, "Target : %d.%d -- Pot : %d -- Min : %d -- Max : %d\n", target, param_idx, potentiometer, min, max);
 	return 0;
 }
 	
