03a9afaba465a425132fcb975e59e4fee88758cd
TomatoPi <dago.esaie@outlook.fr>
2017-05-30 23:44:39 +0200

    Update Modules.cpp

--- source/core/Modules.cpp
+++ source/core/Modules.cpp
@@ -72,46 +72,49 @@ Module::Module(const char *server, MODULE_TYPE type, int pc, int ai, int ao, int
 	this->name = (char*)name;
 	
 	//register module ports
-	va_list arg;
 	int port_count = ai + ao + mi + mo;
-	this->port = (jack_port_t**)malloc(port_count * sizeof(jack_port_t*));
-	if(this->port == NULL){
-		fprintf(stderr, "Failed creating ports array\n");
-		exit(1);
-	}
-	va_start(arg, mo);
-	for(int i = 0; i < port_count; i++){
-		int c = 0;
-		if(i < (ai+ao+mi+mo))c = 3;
-		if(i < (ai+ao+mi))c = 2;
-		if(i < (ai+ao))c = 1;
-		if(i < ai)c = 0;
+	if(port_count != 0){
 		
-		jack_port_t *port;
-		const char *n = va_arg(arg, char*);
-		
-		switch(c){
-			case 0:
-				port = jack_port_register( this->client, n, JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
-				break;
-			case 1:
-				port = jack_port_register( this->client, n, JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
-				break;
-			case 2:
-				port = jack_port_register( this->client, n, JACK_DEFAULT_MIDI_TYPE, JackPortIsInput, 0);
-				break;
-			case 3:
-				port = jack_port_register( this->client, n, JACK_DEFAULT_MIDI_TYPE, JackPortIsOutput, 0);
-				break;
-		}
-		
-		if(port == NULL){
-			fprintf(stderr	, "No more port aviable\n");
+		va_list arg;
+		this->port = (jack_port_t**)malloc(port_count * sizeof(jack_port_t*));
+		if(this->port == NULL){
+			fprintf(stderr, "Failed creating ports array\n");
 			exit(1);
 		}
-		this->port[i] = port;
+		va_start(arg, mo);
+		for(int i = 0; i < port_count; i++){
+			int c = 0;
+			if(i < (ai+ao+mi+mo))c = 3;
+			if(i < (ai+ao+mi))c = 2;
+			if(i < (ai+ao))c = 1;
+			if(i < ai)c = 0;
+
+			jack_port_t *port;
+			const char *n = va_arg(arg, char*);
+
+			switch(c){
+				case 0:
+					port = jack_port_register( this->client, n, JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
+					break;
+				case 1:
+					port = jack_port_register( this->client, n, JACK_DEFAULT_AUDIO_TYPE, JackPortIsOutput, 0);
+					break;
+				case 2:
+					port = jack_port_register( this->client, n, JACK_DEFAULT_MIDI_TYPE, JackPortIsInput, 0);
+					break;
+				case 3:
+					port = jack_port_register( this->client, n, JACK_DEFAULT_MIDI_TYPE, JackPortIsOutput, 0);
+					break;
+			}
+
+			if(port == NULL){
+				fprintf(stderr	, "No more port aviable\n");
+				exit(1);
+			}
+			this->port[i] = port;
+		}
+		va_end(arg);
 	}
-	va_end(arg);
 	
 	//register callback function
 	jack_set_process_callback(client, mod_Process_Callback, this);
