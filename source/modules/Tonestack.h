10d887275a4ca0fb1a46ed10ac2143aec651256f
TomatoPi <dago.esaie@outlook.fr>
2017-05-31 11:53:04 +0200

    Create Tonestack.h

---
+++ source/modules/Tonestack.h
@@ -0,0 +1,30 @@
+#ifndef DEF_TONESTACK_H
+#define DEF_TONESTACK_H
+
+#include "../core/Utility.h"
+#include "../core/Modules.h"
+
+class Tonestack : public Module{
+
+	public:
+		Tonestack(const char *server);
+		~Tonestack();
+		
+		int process(jack_nframes_t nframes, void *arg);
+		int bypass(jack_nframes_t nframes, void *arg);
+		
+	protected:
+		
+		spi_tripole *filter; // EQ ( bass, mid, high )
+		
+		/*
+		*	float freq_low
+		* 	float freq_high
+		*	float gain_low
+		*	float gain_mid
+		*	float gain_high
+		*/
+
+}
+
+#endif
