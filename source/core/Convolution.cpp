14e9f5e565ff937e01ef7f691e635ded1b665a8e
TomatoPi <dago.esaie@outlook.fr>
2017-05-31 16:13:42 +0200

    Update Convolution.cpp

--- source/core/Convolution.cpp
+++ source/core/Convolution.cpp
@@ -1,14 +1,6 @@
 #include "Convolution.h"
-
-Convolution::Convolution(){
-
-}
-
-Convolution::~Convolution(){
-
-}
-		
-sample_t Convolution::do_convolution(sample_t *X, sample_t *H, M){
+	
+sample_t do_convolution(sample_t *X, sample_t *H, M){
 	
 	sample_t yn = 0.0;
 	for(int k = 0; k < M; k++){
