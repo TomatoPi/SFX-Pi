4776f8a71f951c41ac803a2e593b94d0674a8143
TomatoPi <dago.esaie@outlook.fr>
2017-05-31 17:32:34 +0200

    Update Reverb.h

--- source/modules/Reverb.h
+++ source/modules/Reverb.h
@@ -1,4 +1,21 @@
 #ifndef DEF_REVERB_H
 #define DEF_REVERB_H
 
+#include "Delay.h"
+
+class Reverb : public Delay{
+	
+	public:
+		
+		Reverb(const char *server);
+		~Reverb();
+	
+		int process(jack_nframes_t nframes, void *arg);
+		int bypass(jack_nframes_t nframes, void *arg);
+	
+	protected:
+		
+		
+};
+
 #endif
