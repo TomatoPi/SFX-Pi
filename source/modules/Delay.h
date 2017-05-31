d399301e439975946ef91c2d36132f47bd7456dc
TomatoPi <dago.esaie@outlook.fr>
2017-05-31 09:33:28 +0200

    Update Delay.h

--- source/modules/Delay.h
+++ source/modules/Delay.h
@@ -9,6 +9,8 @@
 #define D_DELAY 250
 #define D_FEED 0.5
 
+int delay_Process_Callback(jack_nframes_t nframes, void *u);
+
 class Delay : public Module{
 
 	public:
@@ -16,9 +18,16 @@ class Delay : public Module{
 		~Delay();
 		
 		int process(jack_nframes_t nframes, void *arg);
+		int process_2(jack_nframes_t nframes, void *arg);
 		int bypass(jack_nframes_t nframes, void *arg);
 	
 	protected:
+	
+		jack_client_t *client_2;
+		char *name_2;
+	
+		jack_port_t **private_port;
+		int private_port_count;
 		
 		Ringbuffer *buffer_L, *buffer_R;
 		rng_reader reader_L, reader_R;
