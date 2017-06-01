3a4c46db71025d09904f094a44cab84d2084db90
TomatoPi <dago.esaie@outlook.fr>
2017-06-01 15:23:19 +0200

    Update Convolution.h

--- source/core/Convolution.h
+++ source/core/Convolution.h
@@ -2,7 +2,10 @@
 #define DEF_CONVOLUTION_H
 
 #include "Ringbuffer.h"
+#include "Utility.h"
 
-sample_t do_convolution(sample_t *X, sample_t *H, M);
+sample_t do_convolution(sample_t *X, sample_t *H, int M);
+
+sample_t do_convolution(Ringbuffer *buff, int offset, sample_t *H, int M, int m);
 
 #endif
